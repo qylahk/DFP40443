04b018f5cb8c0b1a41486d291dc7adb406ddf9b0
qylahk <aqlhkhld@gmail.com>
2026-01-22 12:16:13 +0800

    Add files via upload

---
+++ W2/play.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Aqilah";
+echo "hello world $name";
+echo 'This is a full stack class'.$name;
+echo "<h1> My name is Aqilah </h1>";
+echo "<h2> My PA is Miss Athirah </h2>";
+echo "<h3> I'm from Jabatan Komunikasi Maklumat Dan Teknologi </h3>";
+?>
